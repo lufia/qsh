d694a12b7b0fc9a59ec7496eea2f2bd283330bc0
lufia <lufia@lufia.org>
2017-12-15 10:29:30 +0900

    run script with pass by command line args

--- main.go
+++ main.go
@@ -13,14 +13,30 @@ var (
 )
 
 func main() {
+	flag.Parse()
 	log.SetFlags(0)
+	log.SetPrefix("qsh: ")
 
 	if *flagDebug {
 		yyDebug = 2
 	}
+	if flag.NArg() == 0 {
+		Run(os.Stdin, "<stdin>", nil)
+	} else {
+		args := flag.Args()
+		f, err := os.Open(args[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		Run(f, args[0], args[1:])
+		f.Close()
+	}
+}
+
+func Run(f io.Reader, name string, args []string) {
 	var l Lexer
-	fin := bufio.NewReader(os.Stdin)
-	l.Init(fin, "<stdin>")
+	fin := bufio.NewReader(f)
+	l.Init(fin, name)
 	for yyParse(&l) == 0 {
 	}
 	if l.err != nil && l.err != io.EOF {
